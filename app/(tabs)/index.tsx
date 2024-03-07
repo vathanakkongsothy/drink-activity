48dc187647c6e8188cbf5de919ea70c52453edfb
vathanak <vathanakkongsothy35@gmail.com>
2024-03-07 10:37:46 +0700

    Add modal for people approval in TabOneScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { ActivityIndicator, Modal, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { Card, Colors, Image, Slider } from 'react-native-elements';
 import { EvilIcons } from '@expo/vector-icons';
@@ -11,6 +11,7 @@ import { Button } from 'react-native';
 
 export default function TabOneScreen() {
   const [downloadProgress, setDownloadProgress] = React.useState(0);
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   const persons = [
     { name: 'Vathanak', image: 'person1.jpg', description: 'Description about person 1' },
@@ -76,13 +77,81 @@ export default function TabOneScreen() {
           </View>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => console.log('Drunk button pressed')}
+            onPress={() => setModalVisible(true)}
           >
             <Text style={styles.buttonText}>Drunk</Text>
           </TouchableOpacity>
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.modalBackground}>
+              <View style={styles.modalContainer}>
+                <Card>
+                  <Card.Title>
+                    <Text>People Approval 3 / 3</Text>
+                  </Card.Title>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View style={{ marginLeft: '-1rem' }} >
+                      <CheckBox checked={true} />
+                    </View>
+                    <Text style={{ fontWeight: 'bold', marginLeft: '-1rem' }}>Vathanak</Text>
+                    <Text style={{ marginLeft: '2.5rem' }}>{new Date().toLocaleString()}</Text>
+                  </View>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View style={{ marginLeft: '-1rem' }} >
+                      <CheckBox checked={true} />
+                    </View>
+                    <Text style={{ fontWeight: 'bold', marginLeft: '-1rem' }}>The Seng</Text>
+                    <Text style={{ marginLeft: '2.5rem' }}>{new Date().toLocaleString()}</Text>
+                  </View>
+
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View style={{ marginLeft: '-1rem' }} >
+                      <CheckBox checked={true} />
+                    </View>
+                    <Text style={{ fontWeight: 'bold', marginLeft: '-1rem' }}>The Phea</Text>
+                    <Text style={{ marginLeft: '2.5rem' }}>{new Date().toLocaleString()}</Text>
+                  </View>
+                </Card>
+                <View style={{ flexDirection: 'row', alignItems: 'center', gap: '0.5rem' }}>
+                  {true ? (<TouchableOpacity
+                    style={true ? styles.disabledButton : styles.button}
+                    onPress={() => setModalVisible(!modalVisible)}
+                    disabled={true}
+                  >
+                    <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: 'transparent' }}>
+                      <Text style={{ backgroundColor: 'transparent', marginRight: 8 }}>Waiting Approval</Text>
+                      <ActivityIndicator size="small" />
+                    </View>
+                  </TouchableOpacity>) : <TouchableOpacity
+                    style={styles.closeButton}
+                    onPress={() => console.log('Submit button pressed')}
+                  >
+                    <Text style={styles.buttonText}>Submit</Text>
+                  </TouchableOpacity>}
+
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  >
+                    <Text style={styles.buttonText}>Close</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+
         </Card>
       ))}
     </ScrollView>
+
+
   );
 }
 
@@ -101,11 +170,41 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#000',
-    padding: 5, // Reduced padding
+    padding: 10, // Reduced padding
+    borderRadius: 5,
+  },
+  disabledButton: {
+    backgroundColor: '#D3D3D3',
+    padding: 10,
     borderRadius: 5,
   },
   buttonText: {
     color: '#fff',
     textAlign: 'center',
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    width: '80%',
+    padding: 4,
+    alignItems: 'center',
+    gap: 10
+  },
+  closeButton: {
+    backgroundColor: '#2196F3',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    marginRight: 8,
   },
 });
